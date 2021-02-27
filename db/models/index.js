fa590ba8ef3af0d9b6cd58fb626c88f85438205c
guedriOussema <guedroussemamath@gmail.com>
2021-02-27 17:39:31 +0100

    Users & Authentication

--- db/models/index.js
+++ db/models/index.js
@@ -1,7 +1,9 @@
 const { Post } = require('./post.model');
 const { Comment } = require('./comment.model');
+const { User } = require('./user.model');
 
 module.exports = {
     Post,
-    Comment
+    Comment,
+    User
 }
